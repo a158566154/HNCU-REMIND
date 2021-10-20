c759450b5d92a183ed3bcb5d9b0cce8edebf10e2
Tonk yang <59275493+a158566154@users.noreply.github.com>
2021-10-20 21:33:40 +0800

    Update server_sent.py

--- server_sent.py
+++ server_sent.py
@@ -1,13 +1,13 @@
 import requests
 from get_cmd import get_data
 
-def server_sent(img):
+def server_sent(meg):
     
 
-    url = 'https://sctapi.ftqq.com/【server酱密钥】.send?title=电费不足提醒,当前电费剩余{}'.format(img) #server酱推送url
+    url = 'https://sctapi.ftqq.com/【server酱密钥】.send?title=电费不足提醒,当前电费剩余{}'.format(meg) #server酱推送url
     
     return requests.get(url).status_code
 
-def wechat_sent(img):
+def wechat_sent(meg):
     
     return 0
