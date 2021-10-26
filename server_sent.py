429463e26a03e421783af516fa52f0b14de7f08a
Tonk yang <59275493+a158566154@users.noreply.github.com>
2021-10-26 15:51:34 +0800

    Update server_sent.py

--- server_sent.py
+++ server_sent.py
@@ -1,6 +1,24 @@
-import requests
+import requests,json,urllib3
 from get_cmd import get_data
+urllib3.disable_warnings()
 
+
+# Corpid是企业ID
+Corpid = ""
+
+# Secret是管理组凭证密钥
+Secret = ""
+
+# 应用ID
+Agentid = ""
+
+# token_config文件放置路径
+Token_config = r'./config.json'
+
+
+
+
+###下面的代码都不需要动###
 def server_sent(meg):
     
 
@@ -8,6 +26,59 @@ def server_sent(meg):
     
     return requests.get(url).status_code
 
-def wechat_sent(meg):
+
+def GetTokenFromServer(Corpid, Secret):
+    """获取access_token"""
+    Url = "https://qyapi.weixin.qq.com/cgi-bin/gettoken"
+    Data = {
+        "corpid": Corpid,
+        "corpsecret": Secret
+    }
+    r = requests.get(url=Url, params=Data, verify=False)
+    print(r.json())
+    if r.json()['errcode'] != 0:
+        return False
+    else:
+        Token = r.json()['access_token']
+        file = open(Token_config, 'w')
+        file.write(r.text)
+        file.close()
+        return Token
+
+
+def SendMessage(Partyid=1, Subject=None, Content=None):
+    """发送消息"""
+    # 获取token信息
+    try:
+        file = open(Token_config, 'r')
+        Token = json.load(file)['access_token']
+        file.close()
+    except:
+        Token = GetTokenFromServer(Corpid, Secret)
+
+    # 发送消息
+    Url = "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=%s" % Token
+    Data = {
+        "toparty": Partyid,
+        "msgtype": "text",
+        "agentid": Agentid,
+        "text": {"content": Subject + '\n' + Content},
+        "safe": "0"
+    }
+    data=json.dumps(Data,ensure_ascii=False)
     
-    return 0
+    r = requests.post(url = Url,data = data.encode(), verify=False)
+
+
+    # 如果发送失败，将重试三次
+    n = 1
+    while r.json()['errcode'] != 0 and n < 4:
+        n = n + 1
+        Token = GetTokenFromServer(Corpid, Secret)
+        if Token:
+            Url = "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=%s" % Token
+            
+            r = requests.post(url=Url, data=json.dumps(Data,ensure_ascii=False), verify=False)
+            print(r.json())
+
+    return r.json()
