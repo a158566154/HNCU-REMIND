91aa3eff5fbb4b95884dc8d16bbe58dd0c56caf4
Tonk yang <59275493+a158566154@users.noreply.github.com>
2021-11-03 11:34:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,35 +6,36 @@ from server_sent import SendMessage
 
 
 def select_money():
-    power_url = 'http://smart.hncu.net/microhome/classic/html/index.html#?publicUserId=admin&weixin_openid=oKAGAwzqqC9AGIMd-rTYDJHaJuEY&tab=5'
-    woter_url = 'http://smart.hncu.net/microhomes/classic/html/index.html#?publicUserId=admin&weixin_openid=oKAGAwzqqC9AGIMd-rTYDJHaJuEY&tab=5'
+    power_url = 'http://smart.hncu.net/microhome/classic/html/index.html#?publicUserId=admin&weixin_openid=oKAGAwzqqC9AGIMd-rTYDJHaJuEY&tab=5\\'
+    woter_url = 'http://smart.hncu.net/microhomes/classic/html/index.html#?publicUserId=admin&weixin_openid=oKAGAwzqqC9AGIMd-rTYDJHaJuEY&tab=5\\'
 
     money = get_data()
     img ='经过查询，当前剩余电费{}元，剩余水费{}元'.format(money[0],money[1])
     print(img)
+    while True:
+        if money[0] < 10 :
 
-    if money[0] < 10 :
+            power_money = img+'电费不足10元，请及时点击下面链接缴纳电费'+power_url
+            #server_sent(power_money)
+            SendMessage(Subject='# 电费不足10元提醒',Content='### 点击<a href=\"{}">这里</a>进行电费缴纳,{}}'.format(power_url,img))
+            print(power_money)
+            time.sleep(3600)
 
-        power_money = img+'电费不足10元，请及时点击下面链接缴纳电费'+power_url
-        #server_sent(power_money)
-        SendMessage(Subject='# 电费不足10元提醒',Content='### 点击<a href=\{}>这里</a>进行电费缴纳'.format(power_url)
-        print(power_money)
-        time.sleep(3600)
+        elif money[1] < 10:
 
-    elif money[1] < 10:
+            woter_money = img+'水费不足10元，请及时点击下面链接缴纳电费'+woter_url
+            print(woter_money)
+            SendMessage(Subject='# 水费不足10元提醒',Content='### 点击<a href=\"{}">这里</a>进行水费缴纳,{}'.format(woter_url,img))
 
-        woter_money = img+'水费不足10元，请及时点击下面链接缴纳电费'+woter_url
-        print(woter_money)
-        SendMessage(Subject='# 水费不足10元提醒',Content='### 点击<a href=\{}>这里</a>进行水费缴纳'.format(woter_url)
-
-        time.sleep(3600)
-    else:
-        #SendMessage(Subject='# 水费不足10元提醒',Content='### 点击<a href=\"http://smart.hncu.net/microhomes/classic/html/index.html#?publicUserId=admin&weixin_openid=oKAGAwzqqC9AGIMd-rTYDJHaJuEY&tab=5\">这里</a>进行水费缴纳')
-        
-        time.sleep(3600)
+            time.sleep(3600)
+        else:
+            SendMessage(Subject='电费不足10元提醒',Content='点击<a href=\"{}">这里</a>进行电费缴纳,{}'.format(power_url,img))
+            
+            time.sleep(3600)
 
 
 if __name__ == '__main__':
-    select_money():
+    
+    select_money()
                     
  
